8103cbce4a6b6664af086f21e8032e4132e3746e
Kitutu <pkitutu@gmail.com>
2016-06-07 16:16:46 +0300

    fix on editing non-numeric form numbers

--- tpl.samples.modify.php
+++ tpl.samples.modify.php
@@ -127,14 +127,14 @@ if($saveChanges || $proceedWithWarningGender || $proceedWithWarningVLRepeatTesti
 	}
 
 	//ensure form number is numeric
-	if(!is_numeric($formNumber)) {
+	/*if(!is_numeric($formNumber)) {
 		$error.="<br /><strong>Form Number '$formNumber' is Not Numeric.</strong><br />The Form Number should be Numeric i.e it should not contain alphanumeric characters e.g A-Z.<br />";
-	}
+	}*/
 
 	//ensure form number is valid
-	/*if(!getDetailedTableInfo2("vl_forms_clinicalrequest","formNumber='$formNumber' or formNumber='".($formNumber/1)."' limit 1","id")) {
+	if(!getDetailedTableInfo2("vl_forms_clinicalrequest","formNumber='$formNumber' or formNumber='".($formNumber/1)."' limit 1","id")) {
 		$error.="<br /><strong>Invalid Form Number '$formNumber'.</strong><br />The Form Number <strong>$formNumber</strong> does not exist in the list of valid Form Numbers.<br /> Kindly input this record with a valid Form Number.<br />";
-	}*/
+	}
 
 	//ensure facility is valid
 	if(!getDetailedTableInfo2("vl_facilities","id='$facilityID'","id")) {
