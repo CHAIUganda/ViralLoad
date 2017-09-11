9415b8e2c2ba73dbbf2388663166b27e5cd41a6b
Kitutu <pkitutu@gmail.com>
2017-09-11 16:43:01 +0300

    limit under pending remved

--- tpl.worksheets.capture.2.php
+++ tpl.worksheets.capture.2.php
@@ -126,8 +126,7 @@ if(mysqlnumrows($num_pending_result0)){
 
 $sql = "SELECT $cols $more_sql 		
 		GROUP BY s.id
-		ORDER BY lrCategory,lrEnvelopeNumber,lrNumericID ASC
-		LIMIT 1000";
+		ORDER BY lrCategory,lrEnvelopeNumber,lrNumericID ASC";
 
 $query = mysqlquery($sql);
 if(mysqlnumrows($query)) {
